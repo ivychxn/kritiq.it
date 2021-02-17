b036a1c518af4cec9c8f7e0ea6407350e5470068
YiyueMaggieMao <myykitty711@gmail.com>
2021-02-17 18:43:55 +0800

    Set up routes for home page and create post page

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom'; 
+
+import Home from './pages/Home/Home';
+import CreatePost from './pages/CreatePost/CreatePost';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Redirect from="/" to="/home" exact></Redirect>
+          <Route path="/home" component={Home}></Route>
+          <Route path="/create-post" component={CreatePost}></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>This is the Navbar</div>
+    )
+}
+
+export default Navbar;
